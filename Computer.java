5fbadc2b4c0181c94aa21c5f2267cce128de6cdd
PreethiElangovan7 <133875934+PreethiElangovan7@users.noreply.github.com>
2023-05-30 17:30:14 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,8 @@
+package week3.day2;
+
+public class Computer {
+	public void computermodel() {
+		System.out.println("computermodel");
+	}
+
+}
---
+++ Desktop.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+public class Desktop extends Computer{
+	public void desktopSize() {
+		System.out.println("desktopsize");
+		
+	}
+	
+	public static void main(String[] args) {
+		Desktop D = new Desktop();
+		D.computermodel();
+		D.desktopSize();
+	}
+
+}
